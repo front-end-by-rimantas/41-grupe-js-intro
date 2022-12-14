3221412b079dd7ea8034ff210b5b85600e4e08dd
Rimantas <belauzas@yahoo.com>
2022-12-14 10:19:41 +0200

    If

--- js/09-if.js
+++ js/09-if.js
@@ -7,6 +7,10 @@ console.clear();
     naudotini: >, <, >=, <=, ===, !==
     NEnaudotini: ==, !=
 
+    Loginiai operatoriai:
+    && - AND - turi tenkinti visas salygas
+    || - OR - turi tenkinti bent viena salygas
+
     Sablonai:
     if () {}
     if () {} else {}
@@ -117,3 +121,156 @@ if (typeof suma === 'number') {
 }
 
 console.log('aaaaa' / 5);
+
+if (NaN === NaN) {
+    console.log('NaN lygus');
+} else {
+    console.log('NaN nelygus');
+}
+
+console.log(5 / 0);
+console.log(0 / 0); // nesamone
+console.log('ewregtrh' / 5); // nesamone
+console.log('ewregtrh' / 'dfgd'); // nesamone
+
+console.clear();
+console.log('CLEAR: 133');
+
+// code nesting
+
+const day = 8;
+
+if (day === 1) {
+    console.log('pirmadienis');
+} else {
+    if (day === 2) {
+        console.log('antradienis');
+    } else {
+        if (day === 3) {
+            console.log('treciadienis');
+        } else {
+            if (day === 4) {
+                console.log('ketvirtadienis');
+            } else {
+                if (day === 5) {
+                    console.log('penktadienis');
+                } else {
+                    if (day === 6) {
+                        console.log('sestadienis');
+                    } else {
+                        if (day === 7) {
+                            console.log('sekmadienis');
+                        } else {
+                            console.log('tokia diena savaiteje neegzistuoja :(');
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+if (day === 1) {
+    console.log('pirmadienis');
+} else if (day === 2) {
+    console.log('antradienis');
+} else if (day === 3) {
+    console.log('treciadienis');
+} else if (day === 4) {
+    console.log('ketvirtadienis');
+} else if (day === 5) {
+    console.log('penktadienis');
+} else if (day === 6) {
+    console.log('sestadienis');
+} else if (day === 7) {
+    console.log('sekmadienis');
+} else {
+    console.log('tokia diena savaiteje neegzistuoja :(');
+}
+
+console.log('end');
+
+// Atrakcionu parkas
+
+// 1)
+// const parkasDirba = true;
+// if (parkasDirba === true) {}
+// if (true === true) {}
+// if (true) {}
+// if (true) { vykdo sita koda } else {}
+
+// 2)
+// const parkasDirba = false;
+// if (parkasDirba === true) {}
+// if (false === true) {}
+// if (false) {}
+// if (false) {} else { vykdo sita koda }
+
+const parkasDirba = false;
+const bijauAukscio = true;
+
+if (parkasDirba) {
+    if (bijauAukscio) {
+        console.log('Maaaaaamaaa!');
+    } else {
+        console.log('Einu i parka - bus smagu!');
+    }
+} else {
+    if (bijauAukscio) {
+        console.log('Parkas nedirba - man pasiseke - liksiu sausomis kelnemis.');
+    } else {
+        console.log('Parkas nedirba. Man liudna, nes labai norejai nueiti :(');
+    }
+}
+
+if (parkasDirba && bijauAukscio) {
+    console.log('Maaaaaamaaa!');
+} else if (parkasDirba && !bijauAukscio) {
+    console.log('Einu i parka - bus smagu!');
+} else if (!parkasDirba && bijauAukscio) {
+    console.log('Parkas nedirba - man pasiseke - liksiu sausomis kelnemis.');
+} else if (!parkasDirba && !bijauAukscio) {
+    console.log('Parkas nedirba. Man liudna, nes labai norejai nueiti :(');
+} else {
+    console.log('Neimanomas pasiekti atvejis!');
+}
+
+
+const amzius = 14;
+const amziausRiba = 12;
+const ugis = 180;
+const ugioRiba = 160;
+const turiuPinigu = 55;
+const kaina = 10;
+
+if (amzius >= amziausRiba) {
+    if (ugis >= ugioRiba) {
+        if (turiuPinigu >= kaina) {
+            console.log('Einu');
+        } else {
+            console.log('Negalima');
+        }
+    } else {
+        console.log('Negalima');
+    }
+} else {
+    console.log('Negalima');
+}
+
+if (amzius >= amziausRiba
+    && ugis >= ugioRiba
+    && turiuPinigu >= kaina) {
+    console.log('Einu');
+} else {
+    console.log('Negalima');
+}
+
+if (amzius < amziausRiba) {
+    console.log('Negalima');
+} else if (ugis < ugioRiba) {
+    console.log('Negalima');
+} else if (turiuPinigu < kaina) {
+    console.log('Negalima');
+} else {
+    console.log('Einu');
+}
