68b5643f32249a2ad533896a308d4375963e52cf
Rimantas <belauzas@yahoo.com>
2022-12-14 11:41:57 +0200

    Shop external file

--- js/10-for.js
+++ js/10-for.js
@@ -106,39 +106,3 @@ console.log(`${student1.name} pazymiu vidurkis yra ${vidurkis1}.`);
 console.log(`${student2.name} pazymiu vidurkis yra ${vidurkis2}.`);
 console.log(`${student3.name} pazymiu vidurkis yra ${vidurkis3}.`);
 console.log(`${student4.name} pazymiu vidurkis yra ${vidurkis4}.`);
-
-console.log('---------------');
-console.clear();
-
-const valiuta = 'EUR';
-const prekes = [
-    {
-        name: 'Bananas',
-        price: 2,
-        inStock: 10
-    },
-    {
-        name: 'Agurkas',
-        price: 3,
-        inStock: 10
-    },
-    {
-        name: 'Pomidoras',
-        price: 1.57,
-        inStock: 30
-    },
-];
-
-function shop() {
-
-}
-
-shop(prekes, valiuta);
-
-// MUSU PARDUOTUVE:
-// ----------------
-// 1) Bananas kainuoja 2.00 EUR ir turime ju 10 vienetu.
-// 2) Agurkas kainuoja 3.00 EUR ir turime ju 10 vienetu.
-// 3) Pomidoras kainuoja 1.57 EUR ir turime ju 30 vienetu.
-// ----------------
-// Viso asortimento kaina: 97.10 EUR.
